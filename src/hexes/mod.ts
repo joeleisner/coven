b242453754d9e4e270b6f866bc931644d45be116
Joel Eisner <jeisner93@gmail.com>
2026-06-12 18:19:18 -0400

    feat(hexes): add $on hex with auto-signal wiring

--- src/hexes/mod.ts
+++ src/hexes/mod.ts
@@ -1,6 +1,7 @@
 export { $attr } from './$attr.ts';
 export { $bewitch } from './$bewitch.ts';
 export { $mut } from './$mut.ts';
+export { $on } from './$on.ts';
 export { $prop } from './$prop.ts';
 export { $scry } from './$scry.ts';
 export { $shdw } from './$shdw.ts';
